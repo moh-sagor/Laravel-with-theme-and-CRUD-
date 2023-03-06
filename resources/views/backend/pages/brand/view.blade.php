91572520691b66bb21d31f8a0c5372e7d1aced66
Sagor Hossain <sagorhassain4@gmail.com>
2023-03-07 01:02:57 +0600

    image view

--- resources/views/backend/pages/brand/view.blade.php
+++ resources/views/backend/pages/brand/view.blade.php
@@ -18,12 +18,41 @@
         <h4>Brand Information</h4>
         <p>{{$brand->name}}</p>
         <p>{{$brand->cat_id}}</p>
-        <p><img height="120" width="150" src="{{asset('backend/assets/brand/'.$brand->image)}}" alt=""></p>
+       
+        <img height="600" width="500" src="{{asset('backend/assets/brand/'.$brand->image)}}" alt="">
     </div>
     <div class="col-md-6">
         <h4>Gallery Images</h4>
         @foreach ($galleries as $gallery)
-        <img height="120" width="150" src="{{asset('backend/assets/brand/gallery/'.$gallery->images)}}" alt="">
+        <style>
+            .gallery{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: relative;
+                margin: 10px;
+                height: 150px;
+                width: 175px;
+
+            }
+            .gallery img{
+                padding:10px;
+                width: 100%;
+                height:100%;
+            }
+            .delete-btn{
+                position:absolute; 
+                right: 1px;
+                top:1px;
+            }
+    
+
+
+        </style>
+        <div class="gallery">
+        <img  src="{{asset('backend/assets/brand/gallery/'.$gallery->images)}}" alt="">
+        <a href="" class="delete-btn btn btn-danger btn-sm">X</a>
+    </div>
         @endforeach
     </div>
 
